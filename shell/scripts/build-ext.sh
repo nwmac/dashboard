ddc8c2cb6c79f3451e7e182f1ae106744f2c65e5
Neil MacDougall <nmacdougall@suse.com>
2021-10-19 11:37:43 +0100

    Plugin build

---
+++ shell/scripts/build-ext.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo $SCRIPT_DIR
+
+BASE_DIR="$( cd $SCRIPT_DIR && cd ../.. & pwd)"
+echo $BASE_DIR
+
+#"vue-cli-service build --name epinio --target lib pkg/epinio/index.js && rm -rf dist-epinio && cp -R dist dist-epinio && cp pkg/epinio/package.json dist-epinio",
+
+PKG_DIST=${BASE_DIR}/dist-pkg/${1}
+
+if [ -d "${BASE_DIR}/pkg/${1}" ]; then
+  echo "Building UI Package $1"
+  rm -rf ${BASE_DIR}/dist-pkg/${1}
+  mkdir -p ${BASE_DIR}/dist-pkg/${1}
+
+  ${BASE_DIR}/node_modules/.bin/vue-cli-service build --name ${1} --target lib pkg/${1}/index.js --dest ${PKG_DIST}
+  # cp -R ${BASE_DIR}/dist/ ${PKG_DIST}/
+  rm ${PKG_DIST}/demo.html
+  cp ${SCRIPT_DIR}/package.json ${PKG_DIST}/package.json
+  sed -i.bak -e "s/@@NAME/${1}/g" ${PKG_DIST}/package.json
+  rm -rf ${PKG_DIST}/*.bak
+fi
