861db00bc2423ae3e02107916aabe92b130d8907
Richa <richa.bisht@suse.com>
2023-11-13 14:49:48 +0100

    Fixed lint

--- shell/config/product/explorer.js
+++ shell/config/product/explorer.js
@@ -329,14 +329,14 @@ export function init(store) {
   });
 
   virtualType({
-    labelKey:         'nav.tools',
-    group:            'cluster',
-    icon:             'globe',
-    namespaced:       false,
-    name:             'c-cluster-explorer-tools',
-    weight:           -2,
-    route:            { name: 'c-cluster-explorer-tools' },
-    exact:            true,
+    labelKey:   'nav.tools',
+    group:      'cluster',
+    icon:       'globe',
+    namespaced: false,
+    name:       'c-cluster-explorer-tools',
+    weight:     -2,
+    route:      { name: 'c-cluster-explorer-tools' },
+    exact:      true,
   });
 
   virtualType({
