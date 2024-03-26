16abd9712443145513fc73497cf64fb163d6547f
Neil MacDougall <nmacdougall@suse.com>
2024-03-26 15:45:36 +0000

    Fix e2e test and lint issues

--- shell/pages/c/_cluster/explorer/index.vue
+++ shell/pages/c/_cluster/explorer/index.vue
@@ -180,7 +180,7 @@ export default {
     showClusterTools() {
       return this.$store.getters['cluster/canList'](CATALOG.CLUSTER_REPO) &&
       this.$store.getters['cluster/canList'](CATALOG.APP);
-    },    
+    },
 
     displayProvider() {
       const other = 'other';
