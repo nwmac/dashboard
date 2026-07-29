f9b59f9a320084b58f07e39203f657cc9edeac79
Neil MacDougall <nmacdougall@suse.com>
2026-07-29 20:55:18 +0100

    Ensure we use markRaw

--- shell/plugins/background-worker/index.js
+++ shell/plugins/background-worker/index.js
@@ -1,8 +1,9 @@
+import { markRaw } from 'vue'
 import workerConstructor from './web-worker.background.ts';
 
 /**
  * Create and link the background worker creator to the store
  */
 export default function({ store }) {
-  store.backgroundCreateWorker = () => new workerConstructor();
+  store.backgroundCreateWorker = markRaw(() => new workerConstructor());
 }
